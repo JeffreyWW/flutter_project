76d8f6836b7769da4d5aba70cc2d8b25fea7c507
Jeffrey <jerreyweiok@163.com>
2019-04-15 23:24:40 +0800

    星星

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -10,13 +12,57 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Welcome to Flutt12er'),
+          backgroundColor: Colors.red,
+          title: new Text(
+            '这是个导航',
+            style: TextStyle(fontSize: 15),
+            textAlign: TextAlign.center,
+          ),
         ),
-        body: Row(
+        body: Column(
           children: <Widget>[
-            new Text('Hello World'),
+            TitleSection(),
+            new Text(
+              'Hello World',
+              textDirection: TextDirection.rtl,
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        //整行元素的对齐方式，下面注释取消掉则星星会顶到头部
+//        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          /*Expanded放在行或者列的里面再包含的元素会填充整个行或者列的剩余空间*/
+          Expanded(
+            child: Column(
+              /*crossAxisAlignment属性为行或者列的子元素对齐方式,默认是貌似是剧中*/
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Text("这是一个title",
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  margin: EdgeInsets.only(bottom: 5),
+                ),
+                Text("这里是文字描述，可以写很多的话，还可以换行来着，你信不信，反正我是信了，换行后需要看下右边的行元素如何布局的"),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          Text("51"),
+        ],
       ),
     );
   }

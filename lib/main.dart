187ab689731b7e9088534b65c43e94929acb9a2f
Jeffrey <jerreyweiok@163.com>
2019-04-17 21:09:54 +0800

    又是一个改动

--- lib/main.dart
+++ lib/main.dart
@@ -105,7 +105,10 @@ class BtnSection extends StatelessWidget {
         }, child: buildBtnColumn(Icons.call, "电话")),
         buildBtnColumn(Icons.near_me, "发送"),
         buildBtnColumn(Icons.share, "分享"),
-        //再来个提交
+
+
+
+        //每个分支提交后只有被应用才能继续提交分支，不然就是不连续的
         //Feature分支
         //Start Feature：从dev基础上fork一个新Feature分支，Feature只是类型，并不是说分支名字叫Feature，
         // 这里需要把分支修改为合适的功能名，并提交到本地

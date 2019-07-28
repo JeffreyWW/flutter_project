264f4623555a274053296189047c66f24c263848
Jeffrey <jeffreyweiok@163.com>
2019-07-28 18:08:09 +0800

    组件之间数据传递

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
+  final String text;
+
+  const HomePage({Key key, this.text}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -12,6 +16,22 @@ class _HomePageState extends State {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(child: (Text("Jeff")),);
+    return Center(
+      child: _Demo(name: "J",),
+    );
   }
+}
+
+
+class _Demo extends StatelessWidget {
+  final name;
+
+  const _Demo({Key key, @required this.name}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Text(name);
+  }
+
 }

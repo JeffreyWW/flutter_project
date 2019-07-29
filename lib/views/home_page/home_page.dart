3852befc84ffcfc5b8f00ba5aaae28405c499ef1
Jeffrey <weirj@belink.com>
2019-07-29 17:54:52 +0800

    异步测试

--- lib/views/home_page/home_page.dart
+++ lib/views/home_page/home_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project/routers/application.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,11 +14,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State {
+  void clickTest() {
+    var list = [];
+
+    for (int i = 0; i < 1000; i++) {
+      list.add(i);
+    }
+
+    Future getValue() {
+      return Future.error(Error());
+
+    }
+
+    getValue().then((v) => print(v)).catchError((e) => print(e));
+
+    print("结束");
+
+//    var httpClient = new HttpClient();
+//    var uri = Uri.parse("https://www.baidu.com");
+//    var uri = Uri.https("www.baidu.com", null);
+//    var request = httpClient.getUrl(uri);
+
+//    var request = httpClient.getUrl();
+
+    print("点我");
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: Center(
-        child: Text("333"),
+        child: FlatButton(
+          onPressed: clickTest,
+          child: Text("点我"),
+        ),
       ),
     );
   }

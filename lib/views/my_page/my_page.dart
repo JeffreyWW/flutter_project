e5b73f2661f0412f7ec5a503300ba39adeb734b1
Jeffrey <jeffreyweiok@163.com>
2019-08-05 00:22:21 +0800

    弹窗

--- lib/views/my_page/my_page.dart
+++ lib/views/my_page/my_page.dart
@@ -7,35 +7,55 @@ class MyPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
+
     return _MyPageState();
   }
 }
 
-class _MyPageState extends State {
+class _MyPageState extends State with SingleTickerProviderStateMixin {
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        duration: const Duration(milliseconds: 500), vsync: this);
+    controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        print("commeing");
+        controller.reset();
+        controller.forward();
+      }
+    });
+    controller.forward();
+  }
+
+  Widget getRotationTransitionWidget() {
+    return RotationTransition(
+      turns: controller,
+      alignment: Alignment.center,
+      child: Text("aaaa"),
+    );
+  }
+
+  Widget sim() {
+    return SimpleDialog(children: <Widget>[],);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: FlatButton(
-          child: Text("clickMe"),
-          onPressed: () {
-            showDialog<void>(
+          child: FlatButton(
+        child: Text("clicMe"),
+        onPressed: () {
+          showDialog(
               context: context,
-              barrierDismissible: true,
-              // false = user must tap button, true = tap outside dialog
-              builder: (BuildContext dialogContext) {
-                return SimpleDialog(
-                  contentPadding: EdgeInsets.all(0),
-                  children: <Widget>[
-                    Container(color: Colors.red, child: Text("Jeff"))
-                  ],
-                );
-              },
-            );
-            ;
-          },
-        ),
-      ),
+              builder: (c) {
+                return Text("Jeff");
+              });
+        },
+      )),
     );
   }
 }

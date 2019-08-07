3a97a2c9a402367bd711999e9a3508ad6d3df59e
Jeffrey <weirj@belink.com>
2019-08-07 16:13:14 +0800

    准备

--- lib/network/network_coder.dart
+++ lib/network/network_coder.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 class NetworkCoder {
   static List<int> requestEncoder(String request, RequestOptions options) {
-    var data =   DefaultTransformer().transformRequest(options);
+
     return [1];
   }
 }
--- lib/network/network_interceptors.dart
+++ lib/network/network_interceptors.dart
@@ -1,8 +1,11 @@
 import 'package:dio/dio.dart';
-///监听模块
+import 'package:flutter_project/network/network_config.dart';
+
+///监听模块,这里可以转换参数的传递
 class NetworkInterceptors {
   static onRequest(RequestOptions options) {
-    print(options.headers);
+    print(options.data);
+
   }
 
   static onResponse(Response response) {
@@ -12,4 +15,23 @@ class NetworkInterceptors {
   static onError(DioError e) {
     print("request error: $e");
   }
+
+  static get customHeader => {
+        "appVersion": "2.1.4",
+        "netWorkTypse": "WIFI",
+        "ipAddress": "fe80::9e:fe6b:15da:a1ca",
+        "mobileType": "iPhoneSimulator",
+        "deviceId": "4A9C4BFC7513AD9D7EE0C27E93F951E1",
+        "sysVersion": "12.2",
+        "resolution": "1242*2208",
+        "platform": "iPhone",
+        "channel": "10001003",
+        "locationInfo": {},
+        "iCIFID": "ynet",
+        "mp_sId": "ynet",
+        "userId": "",
+        "device_id": "ynet",
+        "fingerprint": "",
+        "channelNo": "1",
+      };
 }

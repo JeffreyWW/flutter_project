8c0f38d927fdbbc12a84f84e8d350fc03e6d42a8
Jeffrey <weirj@belink.com>
2019-08-12 14:50:23 +0800

    滑动监听

--- lib/views/home_page/home_page.dart
+++ lib/views/home_page/home_page.dart
@@ -16,11 +16,15 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State {
   void clickTest() {}
   final FoodBloc _foodBloc = FoodBloc();
-  final scrollController = ScrollController();
+  var scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
+    scrollController.addListener(() {
+//
+      print(scrollController.offset);
+    });
   }
 
   ///头部
@@ -103,10 +107,11 @@ class _HomePageState extends State {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             descriptionElements("秒级到账", "assets/icon_shak.png"),
-            Builder(builder: (BuildContext context) {
-              return Text("Jeff");
-
-            },)
+            Builder(
+              builder: (BuildContext context) {
+                return Text("Jeff");
+              },
+            )
           ],
         );
       }
@@ -142,40 +147,56 @@ class _HomePageState extends State {
     }
 
     return Container(
-      child: SafeArea(
-        child: Column(
-//          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            navigationBar(),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: Image(
-                      image: AssetImage("assets/logo.png"),
-                      height: 24.5,
-                      width: 24.5,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Text("北银消费金融",
-                      style:
-                          TextStyle(fontSize: 24, color: HexColor("#FFFFFF"))),
-                ],
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            left: 0,
+            right: 0,
+            child: Transform.scale(
+              scale: 1,
+              child: Image(
+                width: 500,
+                fit: BoxFit.fill,
+                image: AssetImage("assets/bg_home_headerA.png"),
               ),
             ),
-            Container(child: card(context))
-          ],
-        ),
+          ),
+          SafeArea(
+            child: Column(
+//          mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                navigationBar(),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: Image(
+                          image: AssetImage("assets/logo.png"),
+                          height: 24.5,
+                          width: 24.5,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Text("北银消费金融",
+                          style: TextStyle(
+                              fontSize: 24, color: HexColor("#FFFFFF"))),
+                    ],
+                  ),
+                ),
+                Container(child: card(context))
+              ],
+            ),
+          )
+        ],
       ),
       height: 400,
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("assets/bg_home_headerA.png"),
-              fit: BoxFit.cover)),
+//      decoration: BoxDecoration(
+//          image: DecorationImage(
+//              image: AssetImage("assets/bg_home_headerA.png"),
+//              fit: BoxFit.cover)),
     );
   }
 
@@ -236,6 +257,8 @@ class _HomePageState extends State {
             bloc: _foodBloc,
             builder: (BuildContext context, FoodState state) {
               return ListView.builder(
+                controller: scrollController,
+                physics: AlwaysScrollableScrollPhysics(),
                 padding: EdgeInsets.only(top: 0),
                 itemCount: 5,
                 itemBuilder: (BuildContext context, int index) {

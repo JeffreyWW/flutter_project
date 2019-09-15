2ac6fc70f5079ffe63fef31fff77dfed6f59ad7a
Jeffrey <jeffreyweiok@163.com>
2019-09-15 17:34:37 +0800

    login

--- lib/views/login_page/login_page.dart
+++ lib/views/login_page/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project/application.dart';
 import 'package:flutter_project/utils/color.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -16,15 +17,75 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Container(
-            width: ScreenUtil().width,
-            child: Image(
-              fit: BoxFit.cover,
-              image: AssetImage('assets/bg_login_header.png'),
+          _header(context),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(55, 30, 55, 0),
+            child: TextField(
+              decoration: InputDecoration(
+                  prefixIcon: Padding(
+                    ///默认是有边距的,这里可以设置
+                    padding: const EdgeInsets.only(right: 33),
+                    child: Icon(Icons.phone),
+                  ),
+                  hintText: "请输入手机号",
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 0.5))),
             ),
           )
         ],
       ),
+    );
+  }
+
+  Stack _header(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          width: ScreenUtil().width,
+          child: Image(
+            fit: BoxFit.cover,
+            image: AssetImage('assets/bg_login_header.png'),
+          ),
+        ),
+        SafeArea(
+          child: Column(
+            children: <Widget>[
+              Container(
+                  alignment: Alignment.centerLeft,
+                  height: ScreenUtil().setHeight(44),
+                  child: FlatButton(
+                    onPressed: () {
+                      Application.router.pop(context);
+                    },
+                    child: Text(
+                      '取消',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )),
+              Padding(
+                padding: const EdgeInsets.only(top: 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Image(
+                        image: AssetImage("assets/logo.png"),
+                        height: 24.5,
+                        width: 24.5,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Text("北银消费金融",
+                        style: TextStyle(
+                            fontSize: 24, color: HexColor("#FFFFFF"))),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 
